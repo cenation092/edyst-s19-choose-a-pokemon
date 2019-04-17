e766984c910988bbaa3f5c7639e5564c60a4731d
cenation092 <cenation092@gmail.com>
2019-04-18 03:55:22 +0530

    comments added

--- backend/app.py
+++ backend/app.py
@@ -4,12 +4,18 @@ from flask import jsonify
 
 app = Flask(__name__)
 
+# error handler for invalid url
+@app.errorhandler(404)
+def file_not_found(error= None):
+    return '<h1>404 Error</h1>'
+
+# route for display pokemon data
 @app.route('/api/pokemon')
 def index():
-	data = {}
+	data = {} # dictionary for storing pokemon's data
 	name = ["bulbasaur", "charmander", "squirtle"]
 	data['pokemon'] = name
-	json_data = json.dumps(data)
+	json_data = json.dumps(data) # convert data into JSON format
 	return json_data
 
 if __name__ == '__main__':
